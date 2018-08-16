364451630eb2b862c7cc80012446befe0706eab0
wittawasw 💤 <start@wittawasw.com>
2018-08-16 09:48:48 +0700

    add seed listings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+[ {
+    title: 'For Rent 2 Beds Condo in Watthana, Bangkok, Thailand',
+    description: <<-DESCRIPTION,
+    Thong Lor has become the hot spot in town over the last few years, known for its excellent choice of upmarket and fashionable restaurants, along with plenty of trendy spots to drink the night away. Thong Lor has become the hot spot in town over the last few years, known for its excellent choice of upmarket and fashionable restaurants, along with plenty of trendy spots to drink the night away.'
+    DESCRIPTION
+    beds: 2,
+    baths: 2,
+    rent_price: 60000
+  }, {
+    title: 'For Sale 2 Bed Condo in Watthana, Bangkok, Thailand',
+    description: <<-DESCRIPTION,
+    Modern condo Sukhumvit 39
+    75 sqm., 2 br, 2 bth, good view, nice decor.
+    Cozy and peaceful area, convenient location
+    Brand new condo with fully facility.'
+    DESCRIPTION
+    beds: 2,
+    baths: 2,
+    sale_price: 750000
+  }
+].each do |h|
+  Listing.create(h)
+end
